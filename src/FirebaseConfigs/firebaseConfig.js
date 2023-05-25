1bd137e1f56e423724faca1e7ade9c8957b7a9fc
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-05-25 20:44:46 +0530

    updated firebase configuration

--- src/FirebaseConfigs/firebaseConfig.js
+++ src/FirebaseConfigs/firebaseConfig.js
@@ -5,12 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwIw0c29A6mR1jXy-sMnVi842ulkkJ-TU",
-  authDomain: "sasta-aeeb9.firebaseapp.com",
-  projectId: "sasta-aeeb9",
-  storageBucket: "sasta-aeeb9.appspot.com",
-  messagingSenderId: "121090616764",
-  appId: "1:121090616764:web:9e92ffc10af6c175bdcfb9",
+  apiKey: "AIzaSyCX7n1yeipLRWYg2gom4pHO6Rn4nOpv15o",
+  authDomain: "mini-project-e8608.firebaseapp.com",
+  projectId: "mini-project-e8608",
+  storageBucket: "mini-project-e8608.appspot.com",
+  messagingSenderId: "183577511896",
+  appId: "1:183577511896:web:9132d41d061b693fd594ad",
+  measurementId: "G-N92QP6071K",
 };
 
 const app = initializeApp(firebaseConfig);
