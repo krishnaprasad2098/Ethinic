e5028efaca0e8a957c3c2939d06b7ca07c8401b8
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-05-26 21:56:37 +0530

    added the props type

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,52 +1,55 @@
-import React, { useState, useEffect } from 'react'
-import Navbar from './Navbar'
-import Banner from './Banner'
-import { auth, db } from '../FirebaseConfigs/firebaseConfig'
-import { collection, getDocs, query, where } from 'firebase/firestore'
-import ProductSlider from './Some-Product-Components/ProductSlider'
+import React, { useState, useEffect } from "react";
+import Navbar from "./Navbar";
+import Banner from "./Banner";
+import { auth, db } from "../FirebaseConfigs/firebaseConfig";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import ProductSlider from "./Some-Product-Components/ProductSlider";
 
-import './Home.css'
+import "./Home.css";
 
 const Home = () => {
+  function GetCurrentUser() {
+    const [user, setUser] = useState("");
+    const usersCollectionRef = collection(db, "users");
+    useEffect(() => {
+      auth.onAuthStateChanged((userlogged) => {
+        if (userlogged) {
+          // console.log(userlogged.email)
+          const getUsers = async () => {
+            const q = query(
+              collection(db, "users"),
+              where("uid", "==", userlogged.uid)
+            );
+            // console.log(q);
+            const data = await getDocs(q);
+            setUser(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+          };
+          getUsers();
+        } else {
+          setUser(null);
+        }
+      });
+    }, []);
+    return user;
+  }
+  const loggeduser = GetCurrentUser();
+  // if (loggeduser) { console.log(loggeduser[0]) }
 
-    function GetCurrentUser() {
-        const [user, setUser] = useState("");
-        const usersCollectionRef = collection(db, "users");
-        useEffect(() => {
-            auth.onAuthStateChanged(userlogged => {
-                if (userlogged) {
-                    // console.log(userlogged.email)
-                    const getUsers = async () => {
-                        const q = query(collection(db, "users"), where("uid", "==", userlogged.uid));
-                        // console.log(q);
-                        const data = await getDocs(q);
-                        setUser(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-                    };
-                    getUsers();
-                }
-                else {
-                    setUser(null);
-                }
-            })
-        }, [])
-        return user
-    }
-    const loggeduser = GetCurrentUser();
-    // if (loggeduser) { console.log(loggeduser[0]) }
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Banner></Banner>
+      <div className="slider-head">
+        <p>Limited Time Deals</p>
+      </div>
+      <ProductSlider type={"Utensils"} />
+      <ProductSlider type={"Clothes"} />
+      <ProductSlider type={"Toys"} />
+      <ProductSlider type={"Accessories"} />
+      {/* <Products /> */}
+      {/* <p>{loggeduser ? loggeduser[0].email : "No data"}</p> */}
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <Navbar ></Navbar>
-            <Banner></Banner>
-            <div className="slider-head"><p>Limited Time Deals</p></div>
-            <ProductSlider type={'Mobile'} />
-            <ProductSlider type={'Camera'} />
-            <ProductSlider type={'Laptop'} />
-            <ProductSlider type={'Shoes'} />
-            {/* <Products /> */}
-            {/* <p>{loggeduser ? loggeduser[0].email : "No data"}</p> */}
-        </div>
-    )
-}
-
-export default Home
+export default Home;
