b888ef207b23e77c8fc6dc63d362b66e79f8f2f1
krishnaprasad2098 <payamma.krishna@gmail.com>
2023-05-29 16:51:57 +0530

    styled the button for home page

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import checkoutimg from "./assets/checkout.jpg";
+import Navbar from "./Navbar";
 import "./Checkout.css";
 const Checkout = () => {
   return (
-    <div class="body">
-      <div class="image-container">
+    <div className="body">
+      <Navbar />
+      <div className="image-container">
         <img src={checkoutimg} alt="Order Placed Image" />
       </div>
       <h1>Your Order Has Been Placed</h1>
